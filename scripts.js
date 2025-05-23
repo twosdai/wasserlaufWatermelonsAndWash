543ba84132a85caa12479f0736e962536678b32e
twosdai <daniel.wasserlauf@gmail.com>
2025-05-22 20:08:39 -0400

    Init

---
+++ scripts.js
@@ -0,0 +1,8 @@
+// nav/social placeholders
+document.querySelectorAll('.social a, .logo').forEach(el =>
+    el.addEventListener('click', e => {
+      e.preventDefault();
+      alert('Link not implemented yet!');
+    })
+  );
+  
