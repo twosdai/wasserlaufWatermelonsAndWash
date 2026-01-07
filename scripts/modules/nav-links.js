57e42e646e1050450bc0cfd38270761388dbc773
Daniel Wasserlauf <daniel.wasserlauf@gmail.com>
2026-01-06 22:19:35 -0500

    more melons

--- scripts/modules/nav-links.js
+++ scripts/modules/nav-links.js
@@ -1,5 +1,5 @@
 export function initNavLinks() {
-  document.querySelectorAll(".social a, .logo").forEach((el) => {
+  document.querySelectorAll(".social a").forEach((el) => {
     el.addEventListener("click", (event) => {
       event.preventDefault();
       alert("Link not implemented yet!");
